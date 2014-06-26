e6ecddf06fea870a709956d55dec1f43b4f56ca8
Alexandru Niculae <alex2august@gmail.com>
2014-06-26 16:59:45 +0300

    OOP coins

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
+function Coin(x, y, r) {
+  this.x = x;
+  this.y = y;
+  this.r = r;
+  this.drawCoin = function() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+    context.fillStyle = 'white';
+    context.fill();
+  }
+  return this;
+}
+
 var WIDTH = 400,
     HEIGHT = 600,
     COIN_RADIUS = 10;
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+var coin1, coin2, coin3;
 context.canvas.width = WIDTH;
 context.canvas.height = HEIGHT;
 init();
 
 function init(){
-  drawCoin(190, 250, COIN_RADIUS);
-  drawCoin(90, 350, COIN_RADIUS);
-  drawCoin(290, 350, COIN_RADIUS);
+  coin1 = new Coin(190, 250, COIN_RADIUS);
+  coin1.drawCoin();
+  coin2 = new Coin(90, 350, COIN_RADIUS);
+  coin2.drawCoin();
+  coin3 = new Coin(290, 350, COIN_RADIUS);
+  coin3.drawCoin();
 }
-function drawCoin(x, y, r){
-  context.beginPath();
-  context.arc(x, y, r, 0, 2 * Math.PI, false);
-  context.fillStyle = 'white';
-  context.fill();
-}
+
